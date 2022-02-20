e942199e2a9f3768e2745bf5f7b2a20a116d2f55
grpiccoli <guille.arp@gmail.com>
2022-02-20 23:22:30 +1300

    fix errors missing UserInitializer and unnecesary viewimport using

---
+++ BiblioMit/Models/VM/UserInitializerVM.cs
@@ -0,0 +1,19 @@
+﻿namespace BiblioMit.Models.ViewModels
+{
+    public class UserInitializerVM
+    {
+        public UserInitializerVM(IEnumerable<string> roles, IEnumerable<string> claims)
+        {
+            Roles = roles;
+            Claims = claims;
+        }
+        //public string Name { get; set; }
+        public IEnumerable<string> Roles { get; internal set; } = new List<string>();
+        public IEnumerable<string> Claims { get; internal set; } = new List<string>();
+        public string? Email { get; set; }
+        public string? Key { get; set; }
+        //public List<Plataforma> Plataforma { get; } = new List<Plataforma>();
+        public Uri? ImageUri { get; set; }
+        public int? Rating { get; set; }
+    }
+}
