db67dcbeab6315b04f878c9baa45ef9ee7f28f19
grpiccoli <guille.arp@gmail.com>
2022-02-22 14:37:57 +1300

    thrid attemp to fix safari issues

--- BiblioMit/Models/VM/CSPTag.cs
+++ BiblioMit/Models/VM/CSPTag.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Primitives;
 using Microsoft.Net.Http.Headers;
+using System.Linq.Expressions;
 using UAParser;
 
 namespace BiblioMit.Models.VM
@@ -8,36 +9,36 @@ namespace BiblioMit.Models.VM
     {
         public static HashSet<string> BaseUri { get; } = new HashSet<string>
         {
-            "base-uri", "'self'"
+            "'self'"
         };
         public static bool BlockAllMixedContent { get; set; } = true;
         public static HashSet<string> DefaultSrc { get; } = new HashSet<string>
         {
-            "default-src", "'self'"
+            "'self'"
         };
         public static HashSet<string> ConnectSrc { get; } = new HashSet<string>
         {
-            "connect-src", "'self'", "wss://bibliomit:*", "ws://bibliomit:*",
+            "'self'", "wss://bibliomit:*", "ws://bibliomit:*",
             "https://fonts.googleapis.com/", "https://fonts.gstatic.com/",
             "https://www.facebook.com/", "https://web.facebook.com/",
             "https://www.google-analytics.com/"
         };
         public static HashSet<string> FrameSrc { get; } = new HashSet<string>
         {
-            "frame-src", "'self'",
+            "'self'",
             "https://www.facebook.com/", "https://web.facebook.com/"
         };
         public static HashSet<string> ImgSrc { get; } = new HashSet<string>
         {
-            "img-src","data:","blob:","'self'"
+            "data:","blob:","'self'"
         };
         public static HashSet<string> ObjectSrc { get; } = new HashSet<string>
         {
-            "object-src","'none'"
+            "'none'"
         };
         public static HashSet<string> ScriptSrc { get; } = new HashSet<string>
         {
-            "script-src","'self'"
+            "'self'"
         };
         public static HashSet<string> ScriptSrcElem { get; } = new HashSet<string>
         {
@@ -47,7 +48,7 @@ namespace BiblioMit.Models.VM
         };
         public static HashSet<string> StyleSrc { get; } = new HashSet<string>
         {
-            "style-src","'self'", "'unsafe-inline'"
+            "'self'", "'unsafe-inline'"
         };
         public static HashSet<string> StyleSrcElem { get; } = new HashSet<string>
         {
@@ -55,7 +56,7 @@ namespace BiblioMit.Models.VM
         };
         public static HashSet<string> FontSrc { get; } = new HashSet<string>
         {
-            "font-src","'self'","data:","https://fonts.gstatic.com/","https://fonts.googleapis.com/"
+            "'self'","data:","https://fonts.gstatic.com/","https://fonts.googleapis.com/"
         };
         public static bool UpgradeInsecureRequests { get; set; } = true;
         public static HashSet<string> AccessControlUrls { get; } = new HashSet<string>
@@ -85,15 +86,16 @@ namespace BiblioMit.Models.VM
             string styleSrc = string.Empty;
             if (family.Contains("SAFARI") || family.Contains("FIREFOX"))
             {
+                //ScriptSrcElem.Remove("script-src-elem");
+                //StyleSrcElem.Remove("style-src-elem");
                 ScriptSrc.UnionWith(ScriptSrcElem);
                 StyleSrc.UnionWith(StyleSrcElem);
                 if (ScriptSrc.Contains("'unsafe-inline'"))
                 {
                     ScriptSrc.RemoveWhere(s => s.StartsWith("'nonce", StringComparison.Ordinal) || s.StartsWith("'sha", StringComparison.Ordinal));
                 }
-                scriptSrc = string.Join(separator, "script-src", "'self'",
-                    "https://connect.facebook.net/", "https://fonts.googleapis.com/",
-                    ScriptSrc);
+                scriptSrc = string.Join(separator, ScriptSrc.Prepend("script-src"));
+                styleSrc = string.Join(separator, StyleSrc.Prepend("style-src"));
             }
             else
             {
@@ -106,26 +108,26 @@ namespace BiblioMit.Models.VM
                     ScriptSrc.RemoveWhere(s => s.StartsWith("'nonce", StringComparison.Ordinal) || s.StartsWith("'sha", StringComparison.Ordinal));
                 }
                 scriptSrc = string.Join($";{separator}",
-                    string.Join(separator, "script-src", "'self'", ScriptSrc),
-                string.Join(separator, "script-src-elem", "'self'",
-                "https://connect.facebook.net/", "https://fonts.googleapis.com/",
-                ScriptSrcElem));
+                    string.Join(separator, ScriptSrc.Prepend("script-src")),
+                string.Join(separator, ScriptSrcElem.Prepend("script-src-elem")));
+                styleSrc = string.Join($";{separator}",
+                    string.Join(separator, StyleSrc.Prepend("style-src")),
+                string.Join(separator, StyleSrcElem.Prepend("style-src-elem")));
             }
 
+            ConnectSrc.Add($"ws://{baseUrl}");
+
             return string.Join($";{separator}",
-                string.Join(separator, BaseUri),
+                string.Join(separator, BaseUri.Prepend("base-uri")),
                 BlockAllMixedContent ? "block-all-mixed-content" : null,
-                string.Join(separator, DefaultSrc),
-                string.Join(separator, $"ws://{baseUrl}", 
-                ConnectSrc),
-                string.Join(separator, FrameSrc),
-                string.Join(separator, ImgSrc),
-                string.Join(separator, ObjectSrc),
-                string.Join(separator, ScriptSrc),
-                string.Join(separator, "script-src-elem", ScriptSrcElem),
-                string.Join(separator, StyleSrc),
-                string.Join(separator, "style-src-elem", StyleSrcElem),
-                string.Join(separator, FontSrc),
+                string.Join(separator, DefaultSrc.Prepend("default-src")),
+                string.Join(separator, ConnectSrc.Prepend("connect-src")),
+                string.Join(separator, FrameSrc.Prepend("frame-src")),
+                string.Join(separator, ImgSrc.Prepend("img-src")),
+                string.Join(separator, ObjectSrc.Prepend("object-src")),
+                scriptSrc,
+                styleSrc,
+                string.Join(separator, FontSrc.Prepend("font-src")),
                 UpgradeInsecureRequests ? "upgrade-insecure-requests" : null);
         }
         public static string GetAccessControlString() => string.Join(" ", AccessControlUrls);
