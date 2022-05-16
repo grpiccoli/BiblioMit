c21ece3e0107009136b0c4b6ec9e14043fd71ca8
grpiccoli <guille.arp@gmail.com>
2022-05-16 14:29:29 +1200

    print password

--- BiblioMit/Services/PlanktonService.cs
+++ BiblioMit/Services/PlanktonService.cs
@@ -182,7 +182,7 @@ namespace BiblioMit.Services
                     await sw.WriteLineAsync("<br />").ConfigureAwait(false);
                     await sw.WriteLineAsync(HttpUtility.HtmlEncode($"ERROR: Inicio de sesión inválido, verificar credenciales"));
                     await sw.WriteLineAsync("<hr />").ConfigureAwait(false);
-                    LogCouldnotLogIn(_logger, user.Name + " " + signinResponse.Headers.Location?.OriginalString);
+                    LogCouldnotLogIn(_logger, user.Name + " " + user.Password + " " + signinResponse.Headers.Location?.OriginalString);
                 }
             }
             sw.Close();
